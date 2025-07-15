1c550ecd7054ae7954672796e172cf10b9422100
Jardel Thom <jardel_thom@hotmail.com>
2025-07-15 09:38:00 -0300

    configurando rota

--- lib/src/app/app_menu.dart
+++ lib/src/app/app_menu.dart
@@ -4,6 +4,7 @@ import 'package:utilidades/src/views/about_view.dart';
 import 'package:utilidades/src/views/clima_view.dart';
 import 'package:utilidades/src/views/converter_view.dart';
 import 'package:utilidades/src/views/home_view.dart';
+import 'package:utilidades/src/views/product_list_page.dart';
 
 final List<MenuModel> appMenuItems = [
   MenuModel(
@@ -30,4 +31,10 @@ final List<MenuModel> appMenuItems = [
   icon: Icons.thermostat,
   route: '/clima',
   page: TelaClima()),
+
+  MenuModel(title: 'Produtos',
+  icon: Icons.production_quantity_limits,
+  route: '/produtos',
+  page: ProductListPage()),
+
 ];
