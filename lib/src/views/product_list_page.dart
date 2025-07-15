7a27721535cac2f997fcd0277782be360c80d5fb
Jardel Thom <jardel_thom@hotmail.com>
2025-07-15 09:36:25 -0300

    codado product list page

--- lib/src/views/product_list_page.dart
+++ lib/src/views/product_list_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:utilidades/src/controllers/product_controller.dart';
+import 'package:utilidades/src/models/product_model.dart';
 
 class ProductListPage extends StatefulWidget {
   const ProductListPage({super.key});
@@ -8,6 +10,21 @@ class ProductListPage extends StatefulWidget {
 }
 
 class _ProductListPageState extends State<ProductListPage> {
+final _controller = ProductController();
+late Future<List<ProductModel>> _produtos;
+
+@override
+  void initState() {
+    super.initState();
+    _loadProdutos();
+  }
+
+void _loadProdutos(){
+  setState(() {
+    _produtos = _controller.fetchProdutos(context);
+  });
+}
+
   @override
   Widget build(BuildContext context) {
     return const Placeholder();
